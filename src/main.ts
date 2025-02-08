0465f15e06909e34f801b5f24f27ddd8461c300b
Douglas Blank <douglasblank1@gmail.com>
2025-02-07 23:19:47 -0300

    feat(core): improve base css styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createPinia } from 'pinia'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import './styles/index.sass'
 import 'virtual:uno.css'
 
 const app = createApp(App)
