5ad66ce6165f8eeb1c4ba0f3fdeed002fb495295
Douglas Blank <douglasblank1@gmail.com>
2025-03-24 11:39:59 -0300

    feat: implement quasar and tanstack main configurations

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { QueryClient, VueQueryPlugin, type VueQueryPluginOptions } from '@tanstack/vue-query'
 import { createPinia } from 'pinia'
 
-import { Quasar } from 'quasar'
+import { Notify, Quasar } from 'quasar'
+
 import { createApp } from 'vue'
 
 import App from './App.vue'
@@ -11,8 +13,25 @@ import 'virtual:uno.css'
 const app = createApp(App)
 
 app.use(Quasar, {
-  plugins: {},
+  plugins: {
+    Notify,
+  },
 })
+
+const vueQueryOptions: VueQueryPluginOptions = {
+  queryClient: new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        retry: false,
+        staleTime: Infinity,
+      },
+    },
+  }),
+}
+
+app.use(VueQueryPlugin, vueQueryOptions)
 
 app.use(createPinia())
 
