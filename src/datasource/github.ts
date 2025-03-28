f73c66a38f24be3f16ed96dc5c47381793233224
Douglas Blank <douglasblank1@gmail.com>
2025-03-28 11:57:02 -0300

    feat: add new queries to github datasource

--- src/datasource/github.ts
+++ src/datasource/github.ts
@@ -8,3 +8,49 @@ export async function getAllPullRequests() {
     per_page: 100,
   })
 }
+
+export async function approvePullRequest(number: number) {
+  return octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
+    owner: 'multiplierx',
+    repo: 'front',
+    pull_number: number,
+    event: 'APPROVE',
+    body: 'LGTM',
+  })
+}
+
+export async function createShortcutGist() {
+  return octokit.request('POST /gists', {
+    description: 'daily-jira-shortcuts',
+    public: false,
+    files: {
+      'shortcuts.json': {
+        content: '[]',
+      },
+    },
+  })
+}
+
+export async function getGists() {
+  return octokit.request('GET /gists')
+}
+
+export async function getGist(gistId?: string) {
+  if (gistId) {
+    return octokit.request('GET /gists/{gist_id}', {
+      gist_id: gistId,
+    })
+  }
+}
+
+export async function updateGist(gistId: string, content: string) {
+  await octokit.request('PATCH /gists/{gist_id}', {
+    gist_id: gistId,
+    description: 'daily-jira-shortcuts',
+    files: {
+      'shortcuts.json': {
+        content,
+      },
+    },
+  })
+}
