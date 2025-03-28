5a813f8d2ba89372d661edcb8bfb27377e0097c5
Douglas Blank <douglasblank1@gmail.com>
2025-03-28 11:50:16 -0300

    feat: add useMutation to vite auto import

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
           '@tanstack/vue-query': [
             'useQuery',
             'useInfiniteQuery',
+            'useMutation',
           ],
         },
       ],
