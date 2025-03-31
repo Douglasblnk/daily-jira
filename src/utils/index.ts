8e9d4f36a12e909d28c8e4c46a13f412efd19e93
Douglas Blank <douglasblank1@gmail.com>
2025-03-31 11:26:23 -0300

    refactor: export dayjs function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,6 +5,8 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.locale(ptBR)
 dayjs.extend(relativeTime)
 
+export { dayjs }
+
 export function checkIsLink(link: string | undefined): boolean {
   const getValidUrl = /^https?:\/\/(?:www\.)?[-\w@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,63}\b[-\w()@:%\+.~#?&\/=]*$/
   return link ? getValidUrl.test(link) : false
